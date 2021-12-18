fe51c61929b9e7350b3e784937e3cba95ac11945
dnadan2 <_dan_@live.com>
2021-12-18 15:09:30 +0000

    bouyancy and simple boat physics

--- Boat/Lifeboat.gd
+++ Boat/Lifeboat.gd
@@ -1,27 +1,30 @@
 extends RigidBody
 
-const ACCELERATION = Vector3(0,0,1)
-const MAX_SPEED = 5
+const ACCELERATION = 100
 var targetXPos = null
 
-func transitionToXPos(xPos, size):
+func transitionToXPos(xPos, size, delta):
 	if xPos > size / 2:
-		rotation_degrees.y -= 1
+		add_torque(Vector3(0,-50,0) * delta)
 	else:
-		rotation_degrees.y += 1
-	
-	if rotation_degrees.y < 0:
-		rotation_degrees.y = min(-130, rotation_degrees.y)
-	else:
-		rotation_degrees.y = max(130, rotation_degrees.y)
+		add_torque(Vector3(0,50,0) * delta)
 
 func stopTransitioningToXPos():
 	targetXPos = null
 
 func _physics_process(delta):
+	apply_central_impulse(Vector3(0,0,-5) * delta)
+	
 	var velocity = Vector3(sin(rotation_degrees.y),0, cos(rotation_degrees.y)) * 10
 	
-	add_force(ACCELERATION * delta, Vector3.ZERO)
+	if Input.is_mouse_button_pressed(2):
+		var vector = Vector3(sin(rotation.y), 0, cos(rotation.y))
+		apply_central_impulse(vector * ACCELERATION * delta)
 	
 	if Input.is_mouse_button_pressed(1):
-		transitionToXPos(get_viewport().get_mouse_position(), get_viewport().get_size())
+		transitionToXPos(get_viewport().get_mouse_position(), get_viewport().get_size(), delta)
+	
+	var gravity = 0.5
+	var bouyancy = max(0, min(1, (0.17 - translation.y) / 0.17))
+	
+	apply_central_impulse(Vector3(0, (bouyancy - gravity) * 9.8 * 2 * delta, 0))
