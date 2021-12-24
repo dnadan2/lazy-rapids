3c55e264331e6a16e9eecf0d35fbd52367edc834
dnadan2 <_dan_@live.com>
2021-12-24 13:26:49 +0000

    cheers line up with boat

--- Scenery/Finish.gd
+++ Scenery/Finish.gd
@@ -8,5 +8,7 @@ func _on_Area_body_entered(body):
 		GlobalConstants.lifeBoat.finish()
 
 func show_cheers():
+	$Passengers.global_transform.origin.x = GlobalConstants.lifeBoat.global_transform.origin.x
+	
 	for i in range(GlobalConstants.lifeBoat.get_passenger_count()):
 		get_node("Passengers/Cheering"+str(i+1)).visible = true
